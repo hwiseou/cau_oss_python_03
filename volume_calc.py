967eee25fb098af8ed34ffbc57106ad3f307d8ab
hwiseou <127825902+hwiseou@users.noreply.github.com>
2023-03-21 23:21:29 +0900

    Update volume_calc.py
    총 길이에 따른 요금 계산 기능 추가

--- volume_calc.py
+++ volume_calc.py
@@ -6,3 +6,21 @@ height = float(input("높이:"))
 
 volume = width*length*height
 print("박스의 부피는", volume, "입니다.")
+
+ # %% 거리에 따른 박스의 요금 
+  
+total_length = length + width + height
+
+if total_length <= 80: 
+    charge = 5000 
+elif total_length <= 100: 
+    charge = 8000 
+elif total_length <= 120: 
+    charge = 10000 
+elif total_length <= 160 : 
+    charge = 13000 
+else: 
+    charge = "unavailable"
+    
+print("Total length:", total_length)
+print("Charge:", charge)
